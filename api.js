a49b4a6dee5d3026acdee019097c20ca23d95bf5
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-28 18:02:40 +0600

    first commit

---
+++ api.js
@@ -0,0 +1,4 @@
+const user = {
+    id:1, name:'Gorib Amir', job:'actor'
+};
+console.log(user);
---
+++ json.js
@@ -0,0 +1,6 @@
+const user = {
+    id:1, name:'Gorib Amir', job:'actor'
+};
+// JavaSCript Object Notation (JSON)
+
+console.log(user);
