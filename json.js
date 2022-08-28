aa952c46b5516e621563b12a8700a4cd873905d0
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-28 18:17:13 +0600

    Intro to JSON, JSON Structure, parse, stringly

--- json.js
+++ json.js
@@ -2,5 +2,25 @@ const user = {
     id:1, name:'Gorib Amir', job:'actor'
 };
 // JavaSCript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
 
-console.log(user);
+const shop = {
+    owner:'Alia',
+    address:{
+        street:'kocukhet vooter goli',
+        city:'Ranbir',
+        country:'BD'
+    },
+    products:['laptop','mic','monitor','keybord'],
+    revenue: 45000,
+    isOpen:true,
+    isNew: false
+};
+console.log(shop);
+const shopJson = JSON.stringify(shop); // stringly to object create
+console.log(shopJson);
+
+const shopObj = JSON.parse(shopJson); // object to strignly create
+console.log(shopObj);
